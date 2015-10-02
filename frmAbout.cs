64e5ce84abd141127e4bed571e4b0d1ce4fbd48a
Michele Locati <michele@locati.it>
2015-10-02 13:59:08 +0200

    Cleanup

--- frmAbout.cs
+++ frmAbout.cs
@@ -93,7 +93,6 @@ namespace MLocati.MediaData
                                         { }
                                     }
                                 }
-                                
                             }
                             else
                             {
