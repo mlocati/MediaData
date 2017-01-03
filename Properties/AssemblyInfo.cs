f557f9f7788944d41726082f0513d2e8e9a45b7f
Michele Locati <michele@locati.it>
2017-01-03 08:44:17 +0100

    Version 1.3.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MediaData")]
-[assembly: AssemblyCopyright("Copyright © Michele Locati 2015-2016")]
+[assembly: AssemblyCopyright("Copyright © Michele Locati 2015-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("9dc22fb5-db67-42be-a17f-68eeda89afa9")]
 
-[assembly: AssemblyVersion("1.3.1.0")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
